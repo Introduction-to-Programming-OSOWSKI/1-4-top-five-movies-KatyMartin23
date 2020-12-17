81c4a5487b5a319f7396d811cd69d4855272fac8
KatyMartin23 <832006@mystma.org>
2020-12-17 17:23:25 +0000

    topFiveMovies 3

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveMovies(a, b, c, d, e):
     return a + b + c + d + e
-print(topFiveMovies("Hunger Games", "Hunger Games 2", "Hunger Games 3", "Hunger Games 4", "The Maze Runner"))
+print(topFiveMovies("HungerGames", "HungerGames2", "HungerGames3", "HungerGames4", "TheMazeRunner"))
