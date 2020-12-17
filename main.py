4f9ce4b8980dab1044da1054a88c56efbf61a0dc
KatyMartin23 <832006@mystma.org>
2020-12-17 18:02:40 +0000

    topFiveMovies 5

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + e
+    return a + " " + b + " " + c + " " + d + " " + e
 print(topFiveMovies("Movie1", "Movie2", "Movie3", "Movie4", "Movie5"))
